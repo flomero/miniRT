21cb1d3d22c16cc4cb406a5bb496009f789e0b5a
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-06-14 19:15:36 +0200

    Revert changes in sphere and keep notes

--- src/miniRT.c
+++ src/miniRT.c
@@ -6,13 +6,18 @@
 /*   By: klamprak <klamprak@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 18:40:00 by flfische          #+#    #+#             */
-/*   Updated: 2024/06/13 13:17:56 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/06/14 19:07:45 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 	TODO:
 		- for pl 0,0,-10.0 0,2.0,0 255,255,225 with norm > 1 I have mem bug
+		- make sure objects work good with negative and positive values at z
+		- need to properly take care of 0 division, not just return INFINITY
+		- make sure I don't modify vector values in obj functs
+		- checke for mem leaks
+		- merge from master the new stuff
 */
 
 #include "miniRT.h"
--- src/objects/sphere.c
+++ src/objects/sphere.c
@@ -6,20 +6,12 @@
 /*   By: klamprak <klamprak@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/08 17:34:52 by flfische          #+#    #+#             */
-/*   Updated: 2024/06/14 16:52:22 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/06/14 17:10:16 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-// distance = math.sqrt((x - x_c)**2 + (y - y_c)**2 + (z - z_c)**2)
-// b = 2 * (D[0] * (O[0] - C[0]) + D[1] * (O[1] - C[1]) + D[2] * (O[2] - C[2]))
-// c = (O[0] - C[0])**2 + (O[1] - C[1])**2 + (O[2] - C[2])**2 - r**2
-// O = ray_origin
-// D = ray_direction
-// C = sphere_center
-// r = sphere_radius
-// if discriminant < 0 -> No intersection
 /**
  * Checks if a ray hits a sphere.
  *
@@ -29,43 +21,18 @@
  */
 float	ft_sphere_hit(t_object *sphere, t_ray *ray)
 {
-	float		distance;
+	t_vector3	oc;
+	t_vector3	tmp;
 	float		discriminant;
-	t_vector3	*temp;
-	float		a;
-	float		b;
-	float		c;
 
-	a = ft_v3_dotprod(ray->direction, ray->direction);
-	temp = ft_v3_sub(ray->origin, &sphere->pos);
-	b = 2 * ft_v3_dotprod(temp, ray->direction);
-	c = ft_v3_dotprod(temp, temp) - (sphere->s_sphere.diameter / 2.0) * (sphere->s_sphere.diameter / 2.0);
-	discriminant = (b * b) - (4*a*c);
-	if (discriminant >= 0)
-	{
-		distance = sqrt(pow(ray->origin->x - sphere->pos.x, 2)
-			+ pow(ray->origin->y - sphere->pos.y, 2)
-			+ pow(ray->origin->z - sphere->pos.z, 2));
-		return (distance);
-	}
-	else
+	ft_v3_init(&oc, ray->origin->x, ray->origin->y, ray->origin->z);
+	ft_v3_sub_ip(&oc, &sphere->pos);
+	tmp.x = ft_v3_dotprod(ray->direction, ray->direction);
+	tmp.y = 2.0 * ft_v3_dotprod(&oc, ray->direction);
+	tmp.z = ft_v3_dotprod(&oc, &oc) - (sphere->s_sphere.diameter / 2.0)
+		* (sphere->s_sphere.diameter / 2.0);
+	discriminant = tmp.y * tmp.y - 4.0 * tmp.x * tmp.z;
+	if (discriminant < 0)
 		return (INFINITY);
+	return ((-tmp.y - sqrt(discriminant)) / (2.0 * tmp.x));
 }
-
-// float	ft_sphere_hit(t_object *sphere, t_ray *ray)
-// {
-// 	t_vector3	oc;
-// 	t_vector3	tmp;
-// 	float		discriminant;
-
-// 	ft_v3_init(&oc, ray->origin->x, ray->origin->y, ray->origin->z);
-// 	ft_v3_sub_ip(&oc, &sphere->pos);
-// 	tmp.x = ft_v3_dotprod(ray->direction, ray->direction);
-// 	tmp.y = 2.0 * ft_v3_dotprod(&oc, ray->direction);
-// 	tmp.z = ft_v3_dotprod(&oc, &oc) - (sphere->s_sphere.diameter / 2.0)
-// 		* (sphere->s_sphere.diameter / 2.0);
-// 	discriminant = tmp.y * tmp.y - 4.0 * tmp.x * tmp.z;
-// 	if (discriminant < 0)
-// 		return (INFINITY);
-// 	return ((-tmp.y - sqrt(discriminant)) / (2.0 * tmp.x));
-// }
