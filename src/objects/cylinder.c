1dfbb7e8db332d4412036b5e238ed0c7698a5558
flomero <49640251+flomero@users.noreply.github.com>
2024-07-01 17:38:52 +0200

    wip cylinder normal

--- src/objects/cylinder.c
+++ src/objects/cylinder.c
@@ -6,7 +6,7 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 11:45:47 by klamprak          #+#    #+#             */
-/*   Updated: 2024/07/01 11:11:34 by flfische         ###   ########.fr       */
+/*   Updated: 2024/07/01 16:56:45 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,27 +90,63 @@ double	ft_cylinder_hit(t_object *cyl, t_ray *ray)
 	get_plane(&tmp, &top_bot[0], true);
 	calc_abc(ray, &tmp, abc);
 	hit = is_hit_wall_cy(ray, &tmp, abc, &t);
-	hit = hit || inter_disk(ray, &top_bot[1], &t, cyl->s_cylinder.diameter / 2);
-	hit = hit || inter_disk(ray, &top_bot[0], &t, cyl->s_cylinder.diameter / 2);
+	hit = inter_disk(ray, &top_bot[1], &t, cyl->s_cylinder.diameter / 2) || hit;
+	hit = inter_disk(ray, &top_bot[0], &t, cyl->s_cylinder.diameter / 2) || hit;
 	if (hit)
 		return (t);
 	return (INFINITY);
 }
 
+static int	hit_disks(t_ray *ray, t_object *cylinder, double *t, t_hit *hit)
+{
+	t_object	top_bot[2];
+	double		t_tmp;
+	t_vector3	norm;
+	int			disk_hit;
+
+	disk_hit = 0;
+	get_plane(cylinder, &top_bot[1], false);
+	get_plane(cylinder, &top_bot[0], true);
+	t_tmp = INFINITY;
+	if (inter_disk(ray, &top_bot[1], &t_tmp, cylinder->s_cylinder.diameter / 2))
+		if (t_tmp >= *t - EPSILON && t_tmp <= *t + EPSILON)
+			disk_hit = 2;
+	if (inter_disk(ray, &top_bot[0], &t_tmp, cylinder->s_cylinder.diameter / 2))
+		if (t_tmp >= *t - EPSILON && t_tmp <= *t + EPSILON)
+			disk_hit = 1;
+	if (!disk_hit)
+		return (0);
+	ft_v3_init(&norm, hit->p.x - hit->obj->pos.x, hit->p.y - hit->obj->pos.y,
+		hit->p.z - hit->obj->pos.z);
+	ft_v3_normal_ip(&norm);
+	ft_v3_init(&hit->n, norm.x, norm.y, norm.z);
+	if (disk_hit == 2)
+		ft_v3_scalar_ip(&hit->n, -1);
+	return (disk_hit);
+}
+
 int	ft_cylinder_normal(t_hit *hit, t_ray *ray)
 {
-	t_vector3	norm;
+	int			disk_hit;
+	double		len;
+	t_vector3	hit_to_center;
+	t_vector3	proj_hit;
 
 	ft_v3_init(&hit->p, ray->origin->x + ray->direction->x * hit->t,
 		ray->origin->y + ray->direction->y * hit->t, ray->origin->z
 		+ ray->direction->z * hit->t);
-	ft_v3_init(&hit->n, hit->p.x, hit->p.y, hit->p.z);
-	ft_v3_sub_ip(&hit->n, &hit->obj->pos);
-	ft_v3_init(&norm, hit->obj->s_cylinder.normal.x,
-		hit->obj->s_cylinder.normal.y, hit->obj->s_cylinder.normal.z);
-	ft_v3_scalar_ip(&norm, ft_v3_dotprod(&hit->n,
-			&hit->obj->s_cylinder.normal));
-	ft_v3_sub_ip(&hit->n, &norm);
-	ft_v3_normal_ip(&hit->n);
+	disk_hit = hit_disks(ray, hit->obj, &hit->t, hit);
+	if (disk_hit)
+		return (1);
+	ft_v3_init(&hit_to_center, hit->p.x - hit->obj->pos.x, hit->p.y
+		- hit->obj->pos.y, hit->p.z - hit->obj->pos.z);
+	len = ft_v3_dotprod(&hit_to_center, &hit->obj->s_cylinder.normal);
+	proj_hit.x = hit_to_center.x - len * hit->obj->s_cylinder.normal.x;
+	proj_hit.y = hit_to_center.y - len * hit->obj->s_cylinder.normal.y;
+	proj_hit.z = hit_to_center.z - len * hit->obj->s_cylinder.normal.z;
+	ft_v3_normal_ip(&proj_hit);
+	if (ft_v3_dotprod(&proj_hit, ray->direction) < 0)
+		ft_v3_scalar_ip(&proj_hit, -1);
+	ft_v3_init(&hit->n, proj_hit.x, proj_hit.y, proj_hit.z);
 	return (1);
 }
