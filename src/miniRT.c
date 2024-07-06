c80612686d76a29f56b743b5557fd6a8717fee2a
flomero <49640251+flomero@users.noreply.github.com>
2024-07-06 19:29:50 +0200

    add/remove frees

--- src/miniRT.c
+++ src/miniRT.c
@@ -6,7 +6,7 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 18:40:00 by flfische          #+#    #+#             */
-/*   Updated: 2024/07/06 18:47:07 by flfische         ###   ########.fr       */
+/*   Updated: 2024/07/06 19:30:04 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,15 @@ t_program	*ft_get_program(void)
 	static t_program	program;
 
 	return (&program);
+}
+
+void	ft_free_all(void)
+{
+	t_program	*program;
+
+	program = ft_get_program();
+	if (program->objs)
+		free(program->objs);
 }
 
 void	leaks(void)
@@ -37,10 +46,10 @@ int	main(int argc, char **argv)
 	if (DEBUG)
 		print_objs(NULL);
 	program = ft_get_program();
-	if (ft_mlx_init(argv[1]))
-		return (1);
 	if (!ft_assign_mat_tex() || !ft_init_rt(program))
-		return (1);
+		return (ft_free_all(), 1);
+	if (ft_mlx_init(argv[1]))
+		return (ft_free_all(), 1);
 	mlx_key_hook(program->mlx, ft_key_hook, program);
 	if (program->thread_count < 2)
 		mlx_loop_hook(program->mlx, ft_render, program);
